e3211082fee0b9659d5f577efce8ad3752ba66e2
Johan Yindou <jyindou@gmail.com>
2024-02-13 17:08:31 +0100

    Mise à jour de la BDD

--- src/Entity/Booking.php
+++ src/Entity/Booking.php
@@ -2,22 +2,24 @@
 
 namespace App\Entity;
 
-use App\Repository\BookingsRepository;
+use App\Repository\BookingRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: BookingsRepository::class)]
-class Bookings
+#[ORM\Entity(repositoryClass: BookingRepository::class)]
+class Booking
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
     private ?\DateTimeInterface $start_date = null;
 
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
     private ?\DateTimeInterface $end_date = null;
 
     #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
@@ -32,16 +34,16 @@ class Bookings
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $created_at = null;
 
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
     private ?\DateTimeInterface $updated_at = null;
 
     #[ORM\ManyToOne(inversedBy: 'bookings')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?user $user_id = null;
+    private ?Room $room_id = null;
 
     #[ORM\ManyToOne(inversedBy: 'bookings')]
     #[ORM\JoinColumn(nullable: false)]
-    private ?rooms $room_id = null;
+    private ?User $user_id = null;
+
 
     public function getId(): ?int
     {
@@ -125,34 +127,37 @@ class Bookings
         return $this->updated_at;
     }
 
-    public function setUpdatedAt(\DateTimeInterface $updated_at): static
+    public function setUpdatedAt(?\DateTimeInterface $updated_at): static
     {
         $this->updated_at = $updated_at;
 
         return $this;
-
-}
-
-    public function getUserId(): ?user
-    {
-        return $this->user_id;
     }
 
-    public function setUserId(?user $user_id): static
-    {
-        $this->user_id = $user_id;
+    /**
+     * @return Collection<int, Room>
+     */
 
-        return $this;
-    }
-
-    public function getRoomId(): ?rooms
+    public function getRoom_id(): ?Room
     {
         return $this->room_id;
     }
 
-    public function setRoomId(?rooms $room_id): static
+    public function setRoom_id(?Room $room_id): static
     {
         $this->room_id = $room_id;
+
+        return $this;
+    }
+
+    public function getUserId(): ?User
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(?User $user_id): static
+    {
+        $this->user_id = $user_id;
 
         return $this;
     }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,12 +5,12 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\Table(name: '`user`')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -33,7 +33,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 50, nullable: true)]
     private ?string $firstname = null;
 
-    #[ORM\Column(length: 50)]
+    #[ORM\Column(length: 50, nullable: true)]
     private ?string $lastname = null;
 
     #[ORM\Column(length: 255)]
@@ -43,10 +43,21 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $phone = null;
 
     #[ORM\Column(length: 255, nullable: true)]
-    private ?string $address = null;
+    private ?string $adress = null;
 
-    #[ORM\OneToMany(targetEntity: Bookings::class, mappedBy: 'user_id', orphanRemoval: true)]
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $created_at = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $updated_at = null;
+
+    #[ORM\OneToMany(targetEntity: Booking::class, mappedBy: 'user_id', orphanRemoval: true)]
     private Collection $bookings;
+
+    public function __construct()
+    {
+        $this->bookings = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -135,7 +146,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this->lastname;
     }
 
-    public function setLastname(string $lastname): static
+    public function setLastname(?string $lastname): static
     {
         $this->lastname = $lastname;
 
@@ -166,27 +177,51 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    public function getAddress(): ?string
+    public function getAdress(): ?string
     {
-        return $this->address;
+        return $this->adress;
     }
 
-    public function setAddress(?string $address): static
+    public function setAdress(?string $adress): static
     {
-        $this->address = $address;
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updated_at): static
+    {
+        $this->updated_at = $updated_at;
 
         return $this;
     }
 
     /**
-     * @return Collection<int, Bookings>
+     * @return Collection<int, Booking>
      */
     public function getBookings(): Collection
     {
         return $this->bookings;
     }
 
-    public function addBooking(Bookings $booking): static
+    public function addBooking(Booking $booking): static
     {
         if (!$this->bookings->contains($booking)) {
             $this->bookings->add($booking);
@@ -196,7 +231,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    public function removeBooking(Bookings $booking): static
+    public function removeBooking(Booking $booking): static
     {
         if ($this->bookings->removeElement($booking)) {
             // set the owning side to null (unless already changed)
