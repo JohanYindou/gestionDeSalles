79992c933fef6c5b595ef4ff0dce04088a36b91a
hamady <sakhooo71.com>
2024-02-15 13:18:25 +0100

    ajout de paginator

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,20 +3,29 @@
 namespace App\Controller;
 
 use App\Repository\RoomRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'app_home', methods: ['GET'])]
     public function index(
-        RoomRepository $roomRepository
+        Request $request,
+        RoomRepository $roomRepository,
+        PaginatorInterface $paginator,
     ): Response
     {
-        $rooms = $roomRepository->findAll();
+        $rooms = $paginator->paginate(
+        $rooms = $roomRepository->findAll(),
+            $request->query->getInt('page', 1),9
+
+            );
         return $this->render('home/index.html.twig', [
             'rooms' => $rooms,
+            'featuresRooms' => $roomRepository->findAll(),
         ]);
     }
 }
