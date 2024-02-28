f25f4a59c86dc42f1e018be1065ec0f74274d727
Ikrame <ikr.projet@gmail.com>
2024-02-28 10:16:54 +0100

    Mise en place de la fonctionnalité show de la reservation

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\User;
-use App\Entity\Features;
 use App\Entity\Room;
+use App\Entity\User;
 use App\Entity\Booking;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use App\Entity\Features;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use App\Controller\Admin\BookingCrudController;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -48,10 +59,18 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-users', User::class);
         yield MenuItem::linkToCrud('Features', 'fas fa-list', Features::class);
-        yield MenuItem::linkToCrud('Chambres', 'fas fa-home', Room::class);
-        yield MenuItem::linkToCrud('Réservations', 'fas fa-calendar', Booking::class);
+        yield MenuItem::linkToCrud('Salles', 'fas fa-home', Room::class);
+        yield MenuItem::linkToCrud('Réservations', 'fas fa-calendar', Booking::class)
+            ->setController(BookingCrudController::class);
 
         yield MenuItem::linkToRoute('Retour', 'fa fa-sign-out', 'app_home');
         yield MenuItem::linkToLogout('Déconnexion', 'fa fa-sign-out');
+    }
+
+    public function configureActions(): Actions
+    {
+        return Actions::new()->add(Crud::PAGE_INDEX, Action::new('show', 'View')->linkToCrudAction(Crud::PAGE_DETAIL)->setIcon('fa fa-eye'))
+            ->add(Crud::PAGE_INDEX, Action::new('delete', 'Delete')->linkToCrudAction(Crud::PAGE_EDIT)->setCssClass('btn btn-danger')->setIcon('fa fa-trash'))
+            ->add(Crud::PAGE_INDEX, Action::new('edit')->linkToCrudAction(Crud::PAGE_EDIT)->setIcon('fa fa-pencil'));
     }
 }
